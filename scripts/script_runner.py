1f5d652f98f72bb23110554b4f9cdc4c5707137c
Sohil876 <sohil876@gmail.com>
2017-11-28 18:13:49 +0530

    script runner now properly changes directory before executing the script, usefull for some scripts, also now lists all .py files in scripts directory before asking for filename

--- scripts/script_runner.py
+++ scripts/script_runner.py
@@ -3,12 +3,17 @@
 #qpy:console
 
 import os,os.path,sys
+
 #sys.dont_write_bytecode = True
-
+os.chdir('/sdcard/qpython/scripts/')
+filelist = next(os.walk( '.' ))[2]
+print "Python files in your scripts directory:"
+for file in filelist:
+    if file.endswith( ".py" ):
+        print(os.path.join(file))
+print("Input full name of your file:")
 def modcmd(arg):
-  os.system(sys.executable+" ""/sdcard/qpython/scripts/"+arg)
-
-print("Input full script name placed in scripts directory.")
+  os.system(sys.executable+" "+arg)
 while(True):
   cmd=raw_input("-->")
   if (cmd==""): break;
