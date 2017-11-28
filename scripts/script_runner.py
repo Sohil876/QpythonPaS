dcdfb9d0d7e208751da89df5aac5e71c6dd63cad
Sohil876 <sohil876@gmail.com>
2017-11-29 01:44:48 +0530

    removed unneeded imports, no longer needs expressions for file names even if filename has for eg. spaces

--- scripts/script_runner.py
+++ scripts/script_runner.py
@@ -2,18 +2,18 @@
 #qpy:2
 #qpy:console
 
-import os,os.path,sys
+import os,sys
 
-#sys.dont_write_bytecode = True
 os.chdir('/sdcard/qpython/scripts/')
-filelist = next(os.walk( '.' ))[2]
+filelist = next(os.walk('.'))[2]
 print "Python files in your scripts directory:"
 for file in filelist:
-    if file.endswith( ".py" ):
+    if file.endswith(".py"):
         print(os.path.join(file))
 print("Input full name of your file:")
 def modcmd(arg):
-  os.system(sys.executable+" "+arg)
+    arg2 = ('"{}"'.format(arg))
+    os.system(sys.executable+' '+arg2)
 while(True):
   cmd=raw_input("-->")
   if (cmd==""): break;
