7d1cbffa82571a82d3de6afea73196cd11258734
Sohil876 <sohil876@gmail.com>
2017-11-28 16:40:05 +0530

    Added script runner

---
+++ scripts/script_runner.py
@@ -0,0 +1,16 @@
+#-*-coding:utf8;-*-
+#qpy:2
+#qpy:console
+
+import os,os.path,sys
+#sys.dont_write_bytecode = True
+
+def modcmd(arg):
+  os.system(sys.executable+" ""/sdcard/org.qpython.qpy/scripts/"+arg)
+
+print("Input full script name placed in scripts directory.")
+while(True):
+  cmd=raw_input("-->")
+  if (cmd==""): break;
+  modcmd(cmd)
+
